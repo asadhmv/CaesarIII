47eba671048c060f98f325dc3b7f1c8e68681bee
user <corentin.berlot123@gmail.com>
2023-04-07 16:54:51 +0200

    menu en cours

---
+++ Room.py
@@ -0,0 +1,42 @@
+class Room():
+    
+    def __init__(self, nbJoueur, player, id, mdp=False):
+
+        self.players=[player]
+        self.nbJoueur = nbJoueur
+        self.mdp = mdp
+        self.id = id
+
+    def addPlayer(self, player):
+        if(len(self.players)==self.nbJoueur):
+            return "La room est complète"
+        else:
+            self.append(player)
+            return True
+    
+    def removePlayer(self, player):
+        self.players.remove(player)
+    
+
+    def join(self, player, id, mdp = False):
+        if mdp == self.mdp and id == self.id:
+            return self.addPlayer(player)
+        else:
+            return "ID ou mot de passe incorrecte"
+
+
+    # def create(self,room_name, player):
+    #     if room_name in self.rooms:
+    #         return False
+    #     else:
+    #         self.rooms[room_name]= [player]
+    #         print("created room")
+    #         return True
+
+    # def join(self,room_name, player):
+    #     if room_name in self.rooms:
+    #         self.rooms[room_name].append(player)
+    #         print("Exist")
+    #         print(self.rooms)
+    #         return True
+    #     return False
---
+++ components/text.py
@@ -0,0 +1,34 @@
+import pygame as pg
+
+from pygame import Surface, Rect, Color
+from components.component import Component
+from game import utils
+from sounds.sounds import SoundManager
+
+
+class Text:
+
+    def __init__(self, string, size, pos, color, font=None):
+        self.string = string
+        self.size=size
+        self.pos = pos
+        self.color = color
+        if font == None:
+            self.font = pg.font.Font(None, self.size)
+        else:
+            self.font = font
+        
+        self.text = self.font.render(self.string, True, self.color)
+
+    def display(self, screen, pos=False):
+        if not pos:
+            screen.blit(self.text, self.pos)
+        else:
+            screen.blit(self.text, pos)
+
+    def setString(self, string):
+        self.string = string
+        self.text = self.font.render(string, True, self.color)
+
+    def getString(self):
+        return self.string
