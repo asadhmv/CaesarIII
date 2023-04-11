83c88ae507702f043b255285ab25ea995d140f61
user <corentin.berlot123@gmail.com>
2023-04-11 19:01:56 +0200

    autre

--- Online/multiplayer_connection.py
+++ Online/multiplayer_connection.py
@@ -90,8 +90,8 @@ class Multiplayer_connection:
             if buffer is not None and len(buffer)>0:
                 buffer = buffer.decode()
                 if "$#[|Who is Room Creator?|]#$" in buffer:
-                    print(self.amItheCreatorOfRoom())
-                    if self.amItheCreatorOfRoom():
+                    #print(self.amItheCreatorOfRoom())
+                    if self.room.amIcreator():
                         creator_buffer = self.room.get_info_in_buffer()
                         joiningPlayerIp = buffer[28 : ]
                         print(joiningPlayerIp.encode())
